d4d4d7bba73dbf179aec0cf59331224e864a0903
Emmanuel Benson <emmanuel.benson@konga.com>
2020-07-27 06:35:25 +0100

    Update

--- app.js
+++ app.js
@@ -1,25 +1,46 @@
 const express = require("express");
 const app = express();
 
+const { ModelSyncer } = require("./utils/syncModels");
+
+require("dotenv").config();
+
 // Routes
 const adminRoutes = require("./routes/admin");
 const vendorRoutes = require("./routes/vendor");
 const customerRoutes = require("./routes/customer");
+const authRoutes = require("./routes/auth");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+// app.use(express.urlencoded({ extended: true }));
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/admin", adminRoutes);
 app.use("/api/v1/vendor", adminRoutes);
 app.use("/api/v1/customer", adminRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).json({
-    status: "error",
-    data: {
-      message: "Resource not found.",
-    },
-  });
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
 });
 
-app.listen(3000);
+ModelSyncer()
+  .sync()
+  .then(() => {
+    app.listen(process.env.NODE_ENV_PORT || 3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });

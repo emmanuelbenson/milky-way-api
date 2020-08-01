aec40fa76eaf21c1753a215401acd4c579cc7b0a
Emmanuel Benson <emmanuel.benson@konga.com>
2020-08-01 20:35:14 +0100

    Change database connection

--- utils/database.js
+++ utils/database.js
@@ -5,13 +5,13 @@ const config = require("../config/config.json");
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  config.development.database,
-  config.development.username,
-  config.development.password,
+  config.local.database,
+  config.local.username,
+  config.local.password,
   {
-    host: config.development.host,
-    port: config.development.port,
-    dialect: config.development.dialect,
+    host: config.local.host,
+    port: config.local.port,
+    dialect: config.local.dialect,
   }
 );
 

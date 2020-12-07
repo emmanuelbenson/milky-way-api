ad85bdb26294a2896902c9d95a8180435a9792fb
Emmanuel Benson <emmanuel.benson@konga.com>
2020-12-07 20:57:06 +0100

    Change rate limiter

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -2,6 +2,6 @@ const rateLimite = require("express-rate-limit");
 
 exports.limiter = rateLimite({
   windowMs: 5 * 60 * 1000, // 5 minutes
-  max: 10,
+  max: 150,
   message: "Too many attempts. Please try again after an hour",
 });
